bb6211838b49bc4c104cd0e8ef4f517d75c2fd1d
diegoJuarezVelasco <diegoaw591@gmail.com>
2023-05-22 13:21:02 -0600

    Add Google Analytics 4

--- src/App.js
+++ src/App.js
@@ -8,24 +8,23 @@ import Skillset from './components/Skillset';
 import About from './components/About';
 import proyecto_centro_de_verificacion from './pages/proyecto_centro_de_verificacion';
 import proyecto_donnies_guitars from './pages/proyecto_donnies_guitars';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-      <Router>
-        <Header/>
+    <>
+      <Header />
         <Switch>
-          <Route path='/' exact component={About}/>
-          <Route path='/proyectos' exact component={Projects}/>
-          <Route path='/skillset' component={Skillset}/>
-          <Route path='/contacto' component={Contact}/>
-          <Route path='/proyectos/proyecto-centro-de-verificacion' component={proyecto_centro_de_verificacion}/>
-          <Route path='/proyectos/proyecto-donnies-guitars' component={proyecto_donnies_guitars}/>
-        </Switch>        
-        <Footer/>
-      </Router>
-    
-     );
+          <Route path='/' exact component={About} />
+          <Route path='/proyectos' exact component={Projects} />
+          <Route path='/skillset' component={Skillset} />
+          <Route path='/contacto' component={Contact} />
+          <Route path='/proyectos/proyecto-centro-de-verificacion' component={proyecto_centro_de_verificacion} />
+          <Route path='/proyectos/proyecto-donnies-guitars' component={proyecto_donnies_guitars} />
+        </Switch>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import ReactGA from "react-ga4";
+import {BrowserRouter as Router} from "react-router-dom";
 
 import reportWebVitals from './reportWebVitals';
 
+ReactGA.initialize("G-ZS2MCDCDMT");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
+const SendAnalytics = () => {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname
+  })
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(SendAnalytics);
